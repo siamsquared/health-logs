fa6633b3914c6a89d4b718adb0a2d568f47c7379
PALM <thitaporn@siamsquared.com>
2026-01-12 15:45:26 +0700

    Add CI/CD script

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/onehealth/',
+  base: '/onehealth',
   plugins: [
     TanStackRouterVite(),
     react()
